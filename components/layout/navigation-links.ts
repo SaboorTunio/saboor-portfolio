f9c9c8f293118716ab4d3262451f920481b3ae9d
SaboorTunio <saboortunio2021@gmail.com>
2025-12-30 12:33:32 +0500

    Create navigation-links.ts

---
+++ components/layout/navigation-links.ts
@@ -0,0 +1,8 @@
+import { NavigationLink } from './types';
+
+export const navigationLinks: NavigationLink[] = [
+  { name: 'Home', path: '/', isActive: false },
+  { name: 'Skills', path: '#skills', isActive: false },
+  { name: 'Journey', path: '#journey', isActive: false },
+  { name: 'Contact', path: '#contact', isActive: false },
+];
