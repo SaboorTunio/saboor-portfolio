8b4ec4c6397bfc9ad9ff600a5061d8672b7cbbbb
SaboorTunio <saboortunio2021@gmail.com>
2025-12-31 15:08:36 +0500

    Update types.ts

--- components/layout/types.ts
+++ components/layout/types.ts
@@ -1,5 +1,11 @@
 import { ReactNode } from 'react';
 
+export interface NavigationLink {
+  name: string;
+  path: string;
+  isActive?: boolean;
+}
+
 export interface SocialLink {
   name: string;
   icon: ReactNode;
