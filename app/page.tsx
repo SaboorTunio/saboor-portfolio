017824ace3e65db18e9422b6649b952fbb1dd49c
SaboorTunio <saboortunio2021@gmail.com>
2026-01-01 01:10:48 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import CurrentFocus from "@/components/sections/current-focus";
 const geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI Engineer & Full Stack Developer Portfolio",
+  title: "Saboor-Tunio",
   description: "Showcasing expertise in Next.js, Python, and Agentic AI with a futuristic Cyberpunk Glassmorphism aesthetic",
 };
 
